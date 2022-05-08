8d5d3a6d73ed950e671f20bfabd2df3c255de196
zhangguoan <1621184422@qq.com>
2022-05-09 03:19:48 +0800

    fix sh

--- down.sh
+++ down.sh
@@ -2,6 +2,9 @@ make clean
 #replace 123 with your pwd
 echo 123| sudo  chmod -R  777 /dev/ttyUSB0
 make k210
+echo ""
+echo --------down.sh ------
+echo user program entry is: 
 riscv64-unknown-elf-objdump -d obj/riscv-pke |grep main
 
 
