073fdbd249bfaba8800465a33bb9993da284b370
zhangguoan <1621184422@qq.com>
2022-05-13 01:58:59 +0800

    fix user addr

--- kernel/config.h
+++ kernel/config.h
@@ -15,6 +15,6 @@
 // the ending physical address that PKE observes
 #define PHYS_TOP (DRAM_BASE + PKE_MAX_ALLOWABLE_RAM)
 
-#define USER_PROGRAM_ENTRY 0x8002149e
+#define USER_PROGRAM_ENTRY 0x800215f2
 
 #endif
